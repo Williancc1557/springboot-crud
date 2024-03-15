eb19d2e9440639e8c182bb26eb464c3e28d95499
willian <willian.cacoelho@gmail.com>
2024-03-15 14:06:54 -0300

    feat: create product entity mongodb

---
+++ src/main/java/com/products/productsmanager/Model/Product.java
@@ -0,0 +1,21 @@
+package com.products.productsmanager.Model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document("product")
+public class Product {
+    @Id
+    private String id; // UUID
+
+    private String name;
+    private String description;
+    private int price;
+
+    public Product(String id, String name, String description, int price) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+    }
+}
--- src/main/java/com/products/productsmanager/ProductsManagerApplication.java
+++ src/main/java/com/products/productsmanager/ProductsManagerApplication.java
@@ -9,5 +9,4 @@ public class ProductsManagerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ProductsManagerApplication.class, args);
 	}
-
 }
